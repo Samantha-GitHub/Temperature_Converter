be9a89e194f50ae339dd8b4c39fe16206054c63b
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-04-06 20:16:48 +0200

    putting values into variables

--- main.js
+++ main.js
@@ -9,25 +9,29 @@ for (let i = 0; i < inputs.length; i++) {
     input.addEventListener("input", function (event) {
 
         let value = parseFloat(event.target.value);
-        /* console.log(value); */
+        const onePointHeight = 1.8;
+        const twentyTwo = 32;
+
 
         switch (event.target.name) {
             case "celsius":
 
                 /* To convert temperatures in Celsius to Fahrenheit, multiply by 1.8 and add 32. */
 
-                fahrenheit.value = (value * 1.8) + 32;
+                fahrenheit.value = (value * onePointHeight) + twentyTwo;
 
                 break;
 
             case "fahrenheit":
+
                 /* To convert temperatures in Fahrenheit to Celsius, subtract 32 and dive by 1.8  */
 
 
-                celsius.value = (value - 32) / 1.8;
+                celsius.value = (value - twentyTwo) / onePointHeight;
                 break;
 
             default:
+
                 break;
         }
     })
