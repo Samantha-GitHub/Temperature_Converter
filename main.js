7e6151012b3f4cedc97bb6fdfd5e14d257ba9989
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-04-07 13:24:11 +0200

    adding a validator in main.js  with an alert in index.html if validator not true

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const celsius = document.getElementById("celsius");
 const fahrenheit = document.getElementById("fahrenheit");
+const alert = document.getElementById("alert");
 
 const inputs = document.getElementsByClassName("input");
 
@@ -8,31 +9,42 @@ for (let i = 0; i < inputs.length; i++) {
 
     input.addEventListener("input", function (event) {
 
+
         let value = parseFloat(event.target.value);
         const onePointHeight = 1.8;
         const twentyTwo = 32;
+        const regExpNoLetters = /[^a-z]/gm;
 
 
-        switch (event.target.name) {
-            case "celsius":
 
-                /* To convert temperatures in Celsius to Fahrenheit, multiply by 1.8 and add 32. */
+        if (regExpNoLetters.test(celsius.value) && regExpNoLetters.test(fahrenheit.value)) {
 
-                fahrenheit.value = (value * onePointHeight) + twentyTwo;
+            alert.style.display = 'none';
+            console.log(celsius.value);
 
-                break;
+            switch (event.target.name) {
+                case "celsius":
 
-            case "fahrenheit":
+                    //To convert temperatures in Celsius to Fahrenheit, multiply by 1.8 and add 32.
 
-                /* To convert temperatures in Fahrenheit to Celsius, subtract 32 and dive by 1.8  */
+                    fahrenheit.value = (value * onePointHeight) + twentyTwo;
+
+                    break;
+
+                case "fahrenheit":
+
+                    //To convert temperatures in Fahrenheit to Celsius, subtract 32 and dive by 1.8 
 
 
-                celsius.value = (value - twentyTwo) / onePointHeight;
-                break;
+                    celsius.value = (value - twentyTwo) / onePointHeight;
+                    break;
 
-            default:
 
-                break;
+            }
+
+
+        } else {
+            alert.style.display = 'block';
         }
     })
 }
